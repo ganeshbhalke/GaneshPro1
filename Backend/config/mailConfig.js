b63d14d982687d4586b10360563afc9f67ab9e1d
ganeshbhalke <ganeshbhalke2004@gmail.com>
2026-07-23 11:11:33 +0530

    Update Gmail transporter

--- Backend/config/mailConfig.js
+++ Backend/config/mailConfig.js
@@ -2,17 +2,19 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("✅ SMTP Server Ready");
+  }
 });
 
 module.exports = transporter;
